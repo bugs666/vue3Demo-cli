f0fb11837a09a1edaf02eca2272c6da38dfd1750
zhangxiaoxiong <13581839653@163.com>
2022-01-24 17:29:45 +0800

    解决引入elui+后打包报错问题

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     devServer: {
         port: '9090'
-    }
+    },
+    //https://github.com/element-plus/element-plus/issues/5563
+    transpileDependencies: ['element-plus']
 }
